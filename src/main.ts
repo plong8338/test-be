a107d1a0d32b9934448c244563547d7c96ca3860
Quốc Thắng <quocthang036@gmail.com>
2022-12-07 11:04:04 +0700

    commit install dotenv
    commit install dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
-  const port = process.env.PORT;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port, '0.0.0.0', () => {
-    console.log(`Listening on: ${port}`);
-  });
-}
+    const port = env.PORT || 4321;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(port, '0.0.0.0', () => {
+      console.log(`Listening on: ${port}`);
+    });
+  }
 bootstrap();
